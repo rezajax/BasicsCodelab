ff650d658f09f2a7cb8c0527f419173d8e36803f
Reza Jax <rezajaxg@gmail.com>
2022-06-01 16:05:37 +0430

    end of page 8

--- app/src/main/java/app/bidar/basicscodelab/MainActivity.kt
+++ app/src/main/java/app/bidar/basicscodelab/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,23 +27,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    Greetings()
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen {shouldShowOnboarding = false}
+    } else {
+        Greetings()
+    }
 }
 
 @Composable
 fun Greetings(
     names: List<String> = listOf("World", "Earth")
 ) {
-    Surface(
-        modifier = Modifier.padding(vertical = 4.dp),
-        color = MaterialTheme.colors.background
-    ) {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
-            }
+    Column (modifier = Modifier.padding(vertical = 4.dp)){
+        for (name in names) {
+            Greeting(name = name)
         }
-
     }
 }
 
@@ -53,19 +54,23 @@ fun Greeting(name: String) {
 
     val width = if (more) 48.dp else 0.dp
 
-    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-
-
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
 
-            Column(modifier = Modifier.weight(1f).padding(bottom = width)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = width)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = "$name!")
             }
 
             OutlinedButton(onClick = { more = !more }) {
-//                    if (more) Text(text = "Show More") else Text(text = "Show less")
-                Text(if (more) "Show less" else "Show more")
+                if (more) Text(text = "Show More") else Text(text = "Show less")
+                //Text(if (more) "Show less" else "Show more")
             }
         }
 
@@ -77,7 +82,37 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit
+) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(){}
     }
 }
 
